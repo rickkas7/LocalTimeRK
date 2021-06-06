60cd37f369d3294019c615b248ace38f2179b900
rickkas7 <rickk@rickk.com>
2021-06-06 06:29:21 -0400

    change directory for html docs

---
+++ docs/search/all_d.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['parse_58',['parse',['../class_local_time_h_m_s.html#a09357a93ce14e2a78599c6c51365bda7',1,'LocalTimeHMS::parse()'],['../class_local_time_change.html#abdbaac4596a68e4fc7a79dab54e91886',1,'LocalTimeChange::parse()'],['../class_local_time_posix_timezone.html#ae0204d4d6fab4ea0e3caa8fe9e544398',1,'LocalTimePosixTimezone::parse()']]],
+  ['position_59',['position',['../class_local_time_convert.html#a43d0e3cf2c4a16550dcd423554809bff',1,'LocalTimeConvert::position()'],['../class_local_time_convert.html#a0fab4e36297d6df37510528618871323',1,'LocalTimeConvert::Position()']]]
+];
